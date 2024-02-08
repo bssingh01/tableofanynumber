b7cd92bf7e83527544f355d55a5e4b6d02edd487
bssingh01 <86580690+bssingh01@users.noreply.github.com>
2024-02-08 20:14:45 +0530

    Create printTable.js

---
+++ printTable.js
@@ -0,0 +1,15 @@
+let h1 = document.querySelector("h1");
+let div = document.querySelector("div");
+
+function printTable() {
+  let n = prompt("Enter the number:");
+  h1.innerText = h1.innerText + n + ":";
+  for (let i = 1; i <= 10; i++) {
+    const entry = document.createElement("h3");
+    div.appendChild(entry);
+    entry.innerText = `${n * i}`;
+    // console.log(n * i);
+  }
+}
+
+printTable();
